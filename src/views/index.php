6079608f554bc1645b1363702591ca39947515b6
Jettsyy <teazyyy2512@gmail.com>
2024-03-18 20:16:41 +0700

    TF-1 BaseHtml

---
+++ src/views/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../tailwindCss/output.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold text-center">
+        Hello Everyone!
+    </h1>
+    <p class="text-center">
+        Welcome to XuongPHP
+    </p>
+</body>
+
+</html>
