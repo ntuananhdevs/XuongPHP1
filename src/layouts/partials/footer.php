0bc30984177622f3512df2d612abe56a898a2e29
Jettsyy <teazyyy2512@gmail.com>
2024-03-18 21:22:30 +0700

    TF-1 feat/layout

---
+++ src/layouts/partials/footer.php
@@ -0,0 +1,7 @@
+    <!-- Footer -->
+    <footer class="py-4 mt-4 text-center bg-gray-200">
+        <p class="text-sm text-gray-600">© 2024 Admin Panel. All rights reserved.</p>
+    </footer>
+    </body>
+
+    </html>
