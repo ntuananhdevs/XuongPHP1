502e9f6601337ea332d37ff4f56e5736eb3c4971
Jettsyy <teazyyy2512@gmail.com>
2024-03-19 08:18:34 +0700

    TF-1 feat/base layouts

--- apps/src/layouts/partials/header.php
+++ apps/src/layouts/partials/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Page</title>
-    <link href="../../tailwindCss/output.css" rel="stylesheet">
+    <link href="../../../public/tailwindCss/output.css" rel="stylesheet">
 
 </head>
 
